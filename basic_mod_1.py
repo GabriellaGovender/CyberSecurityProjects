7424fc4f9ca3d2ca0871455e20f4deb9b2fcffa6
GabriellaGovender <83596194+GabriellaGovender@users.noreply.github.com>
2024-02-05 21:28:09 +0200

    Update basic_mod_1.py

--- basic_mod_1.py
+++ basic_mod_1.py
@@ -1,4 +1,4 @@
-
+#picoCTF challenge: basic-mod-1
 import string
 input = [165, 248, 94, 346, 299, 73, 198, 221, 313, 137, 205, 87, 336, 110, 186, 69, 223, 213, 216, 216, 177, 138]
 symbols = string.ascii_uppercase + string.digits + "_"
