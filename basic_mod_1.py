af04b12e3c93884dcf4447d963da7f581cc12563
GabriellaGovender <83596194+GabriellaGovender@users.noreply.github.com>
2024-02-05 21:25:51 +0200

    Add files via upload

---
+++ basic_mod_1.py
@@ -0,0 +1,6 @@
+
+import string
+input = [165, 248, 94, 346, 299, 73, 198, 221, 313, 137, 205, 87, 336, 110, 186, 69, 223, 213, 216, 216, 177, 138]
+symbols = string.ascii_uppercase + string.digits + "_"
+flag = "".join(symbols[s%37] for s in input)
+print("picoctf{{{}}}".format(flag))
