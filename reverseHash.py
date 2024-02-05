0d86f1a5abb81ea34477e97185f28b595680d62f
GabriellaGovender <83596194+GabriellaGovender@users.noreply.github.com>
2024-02-05 21:47:07 +0200

    Update reverseHash.py

--- reverseHash.py
+++ reverseHash.py
@@ -1,7 +1,7 @@
 import hashlib
 import sys
 
-default_fileName = '/home/gabi/Documents/Kreativstorm/hashing/rockyou.txt'
+default_fileName = 'rockyou.txt'
 default_message = "a2099f4c2c2de141afb474dfe4b765ce83448100e77f4359314d94807b00862d53316c03963fc60cbdbd7bc6915778f1830f0f4fd9364a4bc71a09c5e83a0a67"
 
 def getFromFile(fileName):
